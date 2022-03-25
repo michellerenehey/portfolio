64c1626c4a98e12c904b1af4ed7981401067256f
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-25 13:05:01 -0700

    installing react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Layout from './views/Layout/Layout';
 import About from './views/About/About';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
-      <Layout>
-        <About />
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <About />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
