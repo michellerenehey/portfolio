29022186fe3b393613f534439c5c38ae855dc18c
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-01 19:03:56 -0700

    Update tech, add sparkle gif

--- src/views/Tech/Tech.jsx
+++ src/views/Tech/Tech.jsx
@@ -1,50 +1,67 @@
 import './Tech.css';
+import sparkle from '../../assets/sparkle.gif';
+import FadeIn from 'react-fade-in';
 
 export default function Tech() {
   return (
-    <div className="tech">
-      <div className="tech-left">
-        <div className="tech-left_languages">
-          <h2>Languages</h2>
-          <p>JavaScript</p>
-          <p>HTML5</p>
-          <p>CSS3</p>
-        </div>
-        <div className="tech-left_environments">
-          <h2>Frameworks, Libraries</h2>
-          <p>React</p>
-          <p>Node.js</p>
-          <p>Express</p>
-          <p>Redux</p>
-        </div>
-      </div>
-      <div className="tech-center">
-        <div className="tech-center_databases">
-          <h2>Database</h2>
-          <p>PostgreSQL</p>
-        </div>
-        <div className="tech-center_practices">
-          <h2>Dev Practices</h2>
-          <p>Pair/Mob Programming</p>
-          <p>TDD</p>
-          <p>REST API</p>
-        </div>
-      </div>
-      <div className="tech-right">
-        <div className="tech-right_testing-suites">
-          <h2>Testing Suites</h2>
-          <p>Jest</p>
-          <p>QUnit</p>
-        </div>
-        <div className="tech-right_tools">
-          <h2>Tools and More</h2>
-          <p>Github</p>
-          <p>Supabase</p>
-          <p>Postman</p>
-          <p>VSCode</p>
-          <p>Heroku</p>
-          <p>Netlify</p>
-        </div>
+    <div classname="Tech">
+      <img src={sparkle} />
+      <div className="tech-section">
+        <FadeIn>
+          <div className="tech-left">
+            <div className="tech-languages">
+              <h2 className="tech-header">Languages</h2>
+              <hr></hr>
+              <p>JavaScript</p>
+              <p>HTML5</p>
+              <p>CSS3</p>
+            </div>
+            <div className="tech-environments">
+              <h2 className="tech-header">Frameworks &amp; Libraries</h2>
+              <hr></hr>
+              <p>React</p>
+              <p>Node.js</p>
+              <p>Express</p>
+              <p>Redux</p>
+            </div>
+          </div>
+        </FadeIn>
+        <FadeIn>
+          <div className="tech-center">
+            <div className="tech-databases">
+              <h2 className="tech-header">Database</h2>
+              <hr></hr>
+              <p>PostgreSQL</p>
+            </div>
+            <div className="tech-tools">
+              <h2 className="tech-header">Tools and More</h2>
+              <hr></hr>
+              <p>Github</p>
+              <p>Supabase</p>
+              <p>Postman</p>
+              <p>VSCode</p>
+              <p>Heroku</p>
+              <p>Netlify</p>
+            </div>
+          </div>
+        </FadeIn>
+        <FadeIn>
+          <div className="tech-right">
+            <div className="tech-testing-suites">
+              <h2 className="tech-header">Testing Suites</h2>
+              <hr></hr>
+              <p>Jest</p>
+              <p>QUnit</p>
+            </div>
+            <div className="tech-practices">
+              <h2 className="tech-header">Dev Practices</h2>
+              <hr></hr>
+              <p>Pair/Mob Programming</p>
+              <p>TDD</p>
+              <p>REST API</p>
+            </div>
+          </div>
+        </FadeIn>
       </div>
     </div>
   );
