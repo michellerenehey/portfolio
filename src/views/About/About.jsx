10e200455854cf6b499bd91c79873667babaa5f4
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-01 18:27:45 -0700

    Replace main image, exchange footer phrases

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,24 +1,51 @@
 import './About.css';
-import image1 from '../../assets/header_orange.gif';
+import FadeIn from 'react-fade-in';
+import image1 from '../../assets/handshake.gif';
 import image2 from '../../assets/header_blue.gif';
 
 export default function About() {
   return (
     <div className="About">
-      <img
-        src={image2}
-        alt="Michelle Nygren shaking hands"
-        className="main-image"
-      />
-      <div className="bio">
-        <p>
-          I am a hyper-focused <strong>full stack software engineer</strong> who
-          is passionate about <strong>people</strong>. I create smart websites
-          that center on <strong>user experience</strong>, utilizing{' '}
-          <strong>clean code</strong> to build <strong>human-centered</strong>{' '}
-          and <strong>empathetic</strong> tech.
-        </p>
-      </div>
+      <FadeIn>
+        <header className="about-name">MICHELLE NYGREN</header>
+      </FadeIn>
+      <FadeIn>
+        <img
+          src={image1}
+          alt="A drawn image of two women shaking hands"
+          className="about-image"
+        />
+      </FadeIn>
+      <FadeIn>
+        <div className="about-bio">
+          <p className="about-bio_description">
+            I am a hyper-focused <strong>full stack software engineer</strong>{' '}
+            who is passionate about <strong>people</strong>. I create smart
+            websites that center on <strong>user experience</strong>, utilizing{' '}
+            <strong>clean code</strong> to build <strong>human-centered</strong>{' '}
+            and <strong>empathetic</strong> tech. I am:
+          </p>
+        </div>
+      </FadeIn>
+      <FadeIn>
+        <div className="strengths">
+          <div className="strengths-item">
+            <p className="strengths-item_word">CURIOUS</p>
+          </div>
+          <div className="strengths-item">
+            <p className="strengths-item_word">EMPATHETIC</p>
+          </div>
+          <div className="strengths-item">
+            <p className="strengths-item_word">ADAPTABLE</p>
+          </div>
+          <div className="strengths-item">
+            <p className="strengths-item_word">DRIVEN</p>
+          </div>
+          <div className="strengths-item">
+            <p className="strengths-item_word">KIND</p>
+          </div>
+        </div>
+      </FadeIn>
     </div>
   );
 }
