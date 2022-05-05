ec59a656dcacc5ef9bed943c18f9117bf5c7824e
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-05-04 20:03:45 -0700

    Update footer with github logo, add mouseover state to logo

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './Footer.css';
+import whiteGit from '../../../assets/github_white.png';
+import blackGit from '../../../assets/github_black.png';
 
 export default function Footer() {
+  const [gitHover, setGitHover] = useState(false);
+
   return (
     <div className="footer">
       <a
         href="https://github.com/michellerenehey"
         target="_blank"
         className="footer-link"
+        onMouseOver={() => setGitHover(true)}
+        onMouseOut={() => setGitHover(false)}
       >
-        Github
+        <img src={gitHover ? whiteGit : blackGit} />
       </a>
       <a
         href="https://www.linkedin.com/in/michellenygren/"
