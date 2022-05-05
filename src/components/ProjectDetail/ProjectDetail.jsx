c050887bd6abf36b6978e50d079229bad29a2910
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-05-05 16:40:41 -0700

    Add sparkle to project component

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,4 +1,5 @@
 import './ProjectDetail.css';
+import sparkle from '../../assets/sparkle.gif';
 
 export default function ProjectDetail({
   src,
@@ -12,35 +13,39 @@ export default function ProjectDetail({
   gif,
 }) {
   return (
-    <div className="projectdetail">
-      <a href={site || code} target="_blank" className="projectdetail-link">
-        <img src={src} alt="project image" className="projectdetail-image" />
-      </a>
-      {/* <h2 className="projectdetail-name">{name}</h2> */}
-      <p className="projectdetail-tech">{tech}</p>
-      {gif && (
-        <img src={gif} alt="gif of project" className="projectdetail-gif" />
-      )}
-      <p className="projectdetail-description">{description}</p>
-      <ul>
-        {list1 && <li className="projectdetail-list">{list1}</li>}
-        <br />
-        {list2 && <li className="projectdetail-list">{list2}</li>}
-      </ul>
-      <div className="projectdetail-links">
-        {site && (
+    <div className="projectcontainer">
+      <img src={sparkle} className="sparkle" />
+      <div className="projectdetail">
+        <a href={site || code} target="_blank" className="projectdetail-link">
+          <img src={src} alt="project image" className="projectdetail-image" />
+        </a>
+        {/* <h2 className="projectdetail-name">{name}</h2> */}
+        <p className="projectdetail-tech">{tech}</p>
+        {gif && (
+          <img src={gif} alt="gif of project" className="projectdetail-gif" />
+        )}
+        <p className="projectdetail-description">{description}</p>
+        <ul>
+          {list1 && <li className="projectdetail-list">{list1}</li>}
+          <br />
+          {list2 && <li className="projectdetail-list">{list2}</li>}
+        </ul>
+        <div className="projectdetail-links">
+          {site && (
+            <button>
+              <a href={site} target="_blank" className="projectdetail-site">
+                LIVE SITE
+              </a>
+            </button>
+          )}
           <button>
-            <a href={site} target="_blank" className="projectdetail-site">
-              LIVE SITE
+            <a href={code} target="blank" className="projectdetail-site">
+              CODE
             </a>
           </button>
-        )}
-        <button>
-          <a href={code} target="blank" className="projectdetail-site">
-            CODE
-          </a>
-        </button>
+        </div>
       </div>
+      <img src={sparkle} className="sparkle" />
     </div>
   );
 }
