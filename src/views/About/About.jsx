2ee1c74661b16478dc94583874bc33ec9b0e36bf
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-05-06 12:37:04 -0700

    Set up about-me page with room for personal information

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,12 +1,18 @@
 import './About.css';
 import headshot from '../../assets/headshot.jpg';
 import me from '../../assets/me.gif';
+import shoe from '../../assets/running.png';
+
 import Spotify from 'react-spotify-embed';
 
 export default function About() {
   return (
     <div className="about">
-      <img src={me} alt="Gif of Michelle dipping feet in water" />
+      <img
+        src={me}
+        alt="Gif of Michelle dipping feet in water"
+        className="about-pic"
+      />
       <div className="about-bio">
         <p>
           Hi! Thanks for visiting my little corner of the internet. I'm a
@@ -21,13 +27,24 @@ export default function About() {
         </p>
         <p>It's nice to see you!</p>
       </div>
-      <div className="about-facts">
-        <div className="fact">CURRENTLY READING</div>
-        <div className="fact">
-          CURRNTLY LISTENING
-          <Spotify link="https://open.spotify.com/playlist/4sxXL9VpKH3oXB9Ll7YuPx?si=21915288d1454246" />
+      <div className="facts-container">
+        <div className="top-facts">
+          <div className="top-fact">
+            <h3>CURRENTLY TRAINING FOR...</h3>
+            <img src={shoe} alt="Line drawing of shoe" />
+            <p>40 mile trail run around Mt. Hood!</p>
+          </div>
+          <div className="top-fact">CURRENTLY LEARNING...</div>
+          <div className="top-fact">CURRENTLY READING...</div>
         </div>
-        <div className="fact">CURRENTLY BUILDING</div>
+        <div className="bottom-facts">
+          <div className="bottom-fact">
+            <h3>CURRENTLY LISTENING TO...</h3>
+            <div clasName="spotify">
+              <Spotify link="https://open.spotify.com/playlist/4sxXL9VpKH3oXB9Ll7YuPx?si=21915288d1454246" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
