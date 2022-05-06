1cd308ae5559e1441c5ab1343abb136d4910fbf4
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-05-06 13:05:19 -0700

    Style personal facts with images and text

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,9 +1,10 @@
 import './About.css';
+import Spotify from 'react-spotify-embed';
 import headshot from '../../assets/headshot.jpg';
 import me from '../../assets/me.gif';
 import shoe from '../../assets/running.png';
-
-import Spotify from 'react-spotify-embed';
+import reading from '../../assets/open_book.png';
+import lightbulb from '../../assets/lightbulb.png';
 
 export default function About() {
   return (
@@ -30,17 +31,25 @@ export default function About() {
       <div className="facts-container">
         <div className="top-facts">
           <div className="top-fact">
+            <h3>CURRENTLY READING...</h3>
+            <img src={reading} alt="Line drawing of open book" />
+            <p>The King Killer Chronicles by Patrick Rothfuss</p>
+          </div>
+          <div className="top-fact">
             <h3>CURRENTLY TRAINING FOR...</h3>
             <img src={shoe} alt="Line drawing of shoe" />
             <p>40 mile trail run around Mt. Hood!</p>
           </div>
-          <div className="top-fact">CURRENTLY LEARNING...</div>
-          <div className="top-fact">CURRENTLY READING...</div>
+          <div className="top-fact">
+            <h3>CURRENTLY LEARNING...</h3>
+            <img src={lightbulb} alt="Line drawing of lightbulb" />
+            <p>Framer Motion, Embroidery</p>
+          </div>
         </div>
         <div className="bottom-facts">
           <div className="bottom-fact">
             <h3>CURRENTLY LISTENING TO...</h3>
-            <div clasName="spotify">
+            <div className="spotify">
               <Spotify link="https://open.spotify.com/playlist/4sxXL9VpKH3oXB9Ll7YuPx?si=21915288d1454246" />
             </div>
           </div>
