45b5348d9050c63445b0a7b7f9f1b06329f18d99
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-05-06 11:50:22 -0700

    Add selfie gif, add spotify playlist component, jot down placeholder bio

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,5 +1,34 @@
 import './About.css';
+import headshot from '../../assets/headshot.jpg';
+import me from '../../assets/me.gif';
+import Spotify from 'react-spotify-embed';
 
 export default function About() {
-  return <div className="about">About Component</div>;
+  return (
+    <div className="about">
+      <img src={me} alt="Gif of Michelle dipping feet in water" />
+      <div className="about-bio">
+        <p>
+          Hi! Thanks for visiting my little corner of the internet. I'm a
+          software engineer in Portland, OR. Before building things on the
+          internet, I was a digital marketer and a pastry chef. I’ve always
+          loved making things and solving problems, and have always done so with{' '}
+          <strong>people</strong> as my motivation.
+        </p>
+        <p>
+          I decided to learn software development because I believed I could use
+          technology to solve bigger, more human-centered problems.
+        </p>
+        <p>It's nice to see you!</p>
+      </div>
+      <div className="about-facts">
+        <div className="fact">CURRENTLY READING</div>
+        <div className="fact">
+          CURRNTLY LISTENING
+          <Spotify link="https://open.spotify.com/playlist/4sxXL9VpKH3oXB9Ll7YuPx?si=21915288d1454246" />
+        </div>
+        <div className="fact">CURRENTLY BUILDING</div>
+      </div>
+    </div>
+  );
 }
